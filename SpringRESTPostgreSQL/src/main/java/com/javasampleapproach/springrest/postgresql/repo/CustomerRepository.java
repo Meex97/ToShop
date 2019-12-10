5ec76aea0a3f8a7ccc1b9d86b609063a2ab2d17e
salvo1196 <salvatore.latino@edu.unito.it>
2019-12-10 12:54:53 +0100

    init

---
+++ SpringRESTPostgreSQL/src/main/java/com/javasampleapproach/springrest/postgresql/repo/CustomerRepository.java
@@ -0,0 +1,19 @@
+package com.javasampleapproach.springrest.postgresql.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.javasampleapproach.springrest.postgresql.model.Customer;
+
+
+//permette tutte le operazioni che possono essere fatte sul repository in protezione
+// deglio oggetti persistenti
+
+//@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findByAge(int age);
+
+
+}
+
