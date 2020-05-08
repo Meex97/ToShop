a0dcf4fa79c4daef26c43169c916c6011781a509
Massimo Donini <massimo.donini@edu.unito.it>
2020-05-08 16:14:46 +0200

    up/down load image

--- base funzionante/SpringBoot-Angular7-Online-Shopping-Store-master/frontend/src/app/pages/create-products/create-products.component.ts
+++ base funzionante/SpringBoot-Angular7-Online-Shopping-Store-master/frontend/src/app/pages/create-products/create-products.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Location} from "@angular/common";
+import {Location} from '@angular/common';
 
-import {ActivatedRoute, Router} from "@angular/router";
-import {ProductInfo} from "../../models/productInfo";
-import {ProductService} from "../../services/product.service";
-import {Observable} from "rxjs";
-import {apiUrl} from "../../../environments/environment";
-import {JwtResponse} from "../../response/JwtResponse";
-import {UserService} from "../../services/user.service";
+import {ActivatedRoute, Router} from '@angular/router';
+import {ProductInfo} from '../../models/productInfo';
+import {ProductService} from '../../services/product.service';
+import {Observable} from 'rxjs';
+import {apiUrl} from '../../../environments/environment';
+import {JwtResponse} from '../../response/JwtResponse';
+import {UserService} from '../../services/user.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-create-products',
@@ -20,10 +21,18 @@ export class CreateProductsComponent implements OnInit {
   product: ProductInfo;
   private currentUser: JwtResponse;
 
+  selectedFile: File;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message: string;
+  imageName: any;
+
   constructor(private productService: ProductService,
               private route: ActivatedRoute,
               private router: Router,
-              private userService: UserService) {
+              private userService: UserService,
+              private httpClient: HttpClient) {
       this.product = new ProductInfo();
   }
 
@@ -51,5 +60,29 @@ export class CreateProductsComponent implements OnInit {
         this.router.navigate(['/seller']);
       },
       e => {});
+  }
+
+  public onFileChanged(event) {
+    // Select File
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    console.log(this.selectedFile);
+
+    // FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    // Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:8080/api/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+          if (response.status === 200) {
+            this.message = 'Image uploaded successfully';
+          } else {
+            this.message = 'Image not uploaded successfully';
+          }
+        }
+      );
   }
 }
--- base funzionante/SpringBoot-Angular7-Online-Shopping-Store-master/frontend/src/app/pages/upload-image/upload-image.component.ts
+++ base funzionante/SpringBoot-Angular7-Online-Shopping-Store-master/frontend/src/app/pages/upload-image/upload-image.component.ts
@@ -12,32 +12,55 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 
 
 export class UploadImageComponent implements  OnInit {
-  selectedFiles: FileList;
-  currentFileUpload: File;
-  progress: { percentage: number } = { percentage: 0 };
 
-  constructor(private uploadService: UploadFileService) { }
+  selectedFile: File;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message: string;
+  imageName: any;
+
+  constructor(private uploadService: UploadFileService,
+              private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
 
-  selectFile(event) {
-    this.selectedFiles = event.target.files;
+  // Gets called when the user selects an image
+  public onFileChanged(event) {
+    // Select File
+    this.selectedFile = event.target.files[0];
   }
+  // Gets called when the user clicks on submit to upload the image
+  onUpload() {
+    console.log(this.selectedFile);
 
-  upload() {
-    this.progress.percentage = 0;
+    // FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
 
-    this.currentFileUpload = this.selectedFiles.item(0);
-    this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
-      if (event.type === HttpEventType.UploadProgress) {
-        this.progress.percentage = Math.round(100 * event.loaded / event.total);
-      } else if (event instanceof HttpResponse) {
-        console.log('File is completely uploaded!');
-      }
-    });
-
-    this.selectedFiles = undefined;
+    // Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:8080/api/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+          if (response.status === 200) {
+            this.message = 'Image uploaded successfully';
+          } else {
+            this.message = 'Image not uploaded successfully';
+          }
+        }
+      );
+  }
+  // Gets called when the user clicks on retieve image button to get the image from back end
+  getImage() {
+    // Make a call to Sprinf Boot to get the Image Bytes.
+    this.httpClient.get('http://localhost:8080/api/image/get/' + this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResonse = res;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
   }
 
 }
